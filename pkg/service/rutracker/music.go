fe45a535feca94e163f3cf38c2b98ea538eabb38
liujiniang <liujiniang12345@126.com>
2021-08-23 21:29:18 +0800

    增加嵌套处理sp-wrap

--- pkg/service/rutracker/music.go
+++ pkg/service/rutracker/music.go
@@ -16,10 +16,9 @@ import (
 
 type Rutracker struct {
 	baseUrl string
-
 }
 
-func NewForumSite() service.IForumSite{
+func NewForumSite() service.IForumSite {
 	return Rutracker{
 		baseUrl: "https://rutracker.org/%s",
 	}
@@ -33,7 +32,7 @@ func (r Rutracker) SubForumParser() func(body io.Reader) (interface{}, error) {
 	}
 }
 
-func (r Rutracker)GetUrl(path string) string  {
+func (r Rutracker) GetUrl(path string) string {
 	if strings.HasPrefix(path, "https://") ||
 		strings.HasPrefix(path, "http://") {
 		return path
@@ -45,7 +44,7 @@ func (r Rutracker)GetUrl(path string) string  {
 
 var _ service.IForumSite = (*Rutracker)(nil)
 
-func (r Rutracker) DetailParser() func(body io.Reader) (interface{}, error){
+func (r Rutracker) DetailParser() func(body io.Reader) (interface{}, error) {
 	return func(body io.Reader) (interface{}, error) {
 		// Load the HTML document
 		doc, err := goquery.NewDocumentFromReader(transform.NewReader(body, charmap.Windows1251.NewDecoder()))
@@ -81,13 +80,11 @@ func (r Rutracker) DetailParser() func(body io.Reader) (interface{}, error){
 		torrentUrl, _ := aTorrent.Attr("href")
 		logrus.Printf("%s,%s,%s,%s", dataShareUrl, dataShareTitle, extraLinkData, text)
 		tags := []string{"Жанр", "Страна исполнителя (группы)", "Год издания", "Аудиокодек", "Тип рипа", "Битрейт аудио", "Продолжительность", "Исполнитель",
-			"Альбом","Страна","Дата выпуска", "Формат", "Битрейт", "Издатель (лейбл)", "Носитель", "Номер по каталогу", "Годы выпуска дисков",
-
-
-			}
+			"Альбом", "Страна", "Дата выпуска", "Формат", "Битрейт", "Издатель (лейбл)", "Носитель", "Номер по каталогу", "Годы выпуска дисков",
+		}
 		tagMap := r.parseTag(text, tags)
 
-		getTag := func(tagMap map[string]string, keys...string) string{
+		getTag := func(tagMap map[string]string, keys ...string) string {
 			for _, key := range keys {
 				if tagMap[key] != "" {
 					return tagMap[key]
@@ -96,7 +93,6 @@ func (r Rutracker) DetailParser() func(body io.Reader) (interface{}, error){
 			return ""
 		}
 
-
 		if strings.Contains(dataShareTitle, "Дискография") ||
 			strings.Contains(dataShareTitle, "дискография") ||
 			strings.Contains(dataShareTitle, "Discography") ||
@@ -104,50 +100,48 @@ func (r Rutracker) DetailParser() func(body io.Reader) (interface{}, error){
 			strings.Contains(dataShareTitle, "Collection") ||
 			strings.Contains(dataShareTitle, "Коллекция") {
 			if (strings.Contains(dataShareTitle, "Collection") ||
-				strings.Contains(dataShareTitle, "Коллекция") ) &&
+				strings.Contains(dataShareTitle, "Коллекция")) &&
 				strings.Contains(dataShareTitle, "VA") {
 				return nil, nil
 			}
 			return &model.ForumDiscographyInfo{
-				Artist: getTag(tagMap, "Исполнитель", "title"),
-				Title: dataShareTitle,
-				Url : dataShareUrl,
-				Years: getTag(tagMap, "Год выпуска диска", "Годы выпуска дисков", "Год издания"),
-				GenreType: getTag(tagMap,"Жанр"),
-				Country: getTag(tagMap,"Страна исполнителя (группы)", "Страна"),
-				BitRate: getTag(tagMap,"Битрейт аудио","Битрейт"),
-				FileType:  getTag(tagMap,"Аудиокодек","Формат"),
-				Duration: tagMap["Продолжительность"],
-				Content: text,
-				MagnetLink: magnetLink,
+				Artist:      getTag(tagMap, "Исполнитель", "title"),
+				Title:       dataShareTitle,
+				Url:         dataShareUrl,
+				Years:       getTag(tagMap, "Год выпуска диска", "Годы выпуска дисков", "Год издания"),
+				GenreType:   getTag(tagMap, "Жанр"),
+				Country:     getTag(tagMap, "Страна исполнителя (группы)", "Страна"),
+				BitRate:     getTag(tagMap, "Битрейт аудио", "Битрейт"),
+				FileType:    getTag(tagMap, "Аудиокодек", "Формат"),
+				Duration:    tagMap["Продолжительность"],
+				Content:     text,
+				MagnetLink:  magnetLink,
 				MagnetTitle: magnetTitle,
-				Torrent: r.GetUrl(torrentUrl),
-				ExtraInfo: r.ParseAlbumInfo(postBody),
+				Torrent:     r.GetUrl(torrentUrl),
+				ExtraInfo:   r.ParseAlbumInfo(postBody),
 			}, nil
 
 		}
 
 		return &model.ForumAlbumInfo{
-				Artist: getTag(tagMap, "Исполнитель", "title"),
-				Name: getTag(tagMap, "Альбом", "title"),
-				Title: dataShareTitle,
-				Url : dataShareUrl,
-				Year: getTag(tagMap, "Дата выпуска"),
-				GenreType: getTag(tagMap,"Жанр"),
-				Country: getTag(tagMap,"Страна исполнителя (группы)", "Страна"),
-				BitRate: getTag(tagMap,"Битрейт аудио","Битрейт"),
-				FileType:  getTag(tagMap,"Аудиокодек","Формат"),
-				Duration: tagMap["Продолжительность"],
-				Content: text,
-				MagnetLink: magnetLink,
-				MagnetTitle: magnetTitle,
-				Torrent: r.GetUrl(torrentUrl),
-			}, nil
-
+			Artist:      getTag(tagMap, "Исполнитель", "title"),
+			Name:        getTag(tagMap, "Альбом", "title"),
+			Title:       dataShareTitle,
+			Url:         dataShareUrl,
+			Year:        getTag(tagMap, "Дата выпуска"),
+			GenreType:   getTag(tagMap, "Жанр"),
+			Country:     getTag(tagMap, "Страна исполнителя (группы)", "Страна"),
+			BitRate:     getTag(tagMap, "Битрейт аудио", "Битрейт"),
+			FileType:    getTag(tagMap, "Аудиокодек", "Формат"),
+			Duration:    tagMap["Продолжительность"],
+			Content:     text,
+			MagnetLink:  magnetLink,
+			MagnetTitle: magnetTitle,
+			Torrent:     r.GetUrl(torrentUrl),
+		}, nil
 
 	}
 }
-
 
 func (r Rutracker) ItemListParser() func(body io.Reader) (interface{}, error) {
 	return func(body io.Reader) (interface{}, error) {
@@ -176,22 +170,22 @@ func (r Rutracker) ItemListParser() func(body io.Reader) (interface{}, error) {
 }
 
 func (r Rutracker) parseTag(text string, tags []string) map[string]string {
-	tagMap := make( map[string]string, 0)
+	tagMap := make(map[string]string, 0)
 
 	lines := strings.Split(text, "\n")
 
-	parseTag := func(line string, ) {
+	parseTag := func(line string) {
 		for _, tag := range tags {
 			if strings.Contains(line, tag) {
-				value := strings.TrimSpace(line[strings.Index(line, ":") + 1:])
+				value := strings.TrimSpace(line[strings.Index(line, ":")+1:])
 
-				if strings.HasSuffix(value, "Треклист:"){
+				if strings.HasSuffix(value, "Треклист:") {
 					value = value[:len(value)-len("Треклист:")]
 					tagMap["type"] = "album"
-				} else if strings.HasSuffix(value, "Tracklist:"){
+				} else if strings.HasSuffix(value, "Tracklist:") {
 					value = value[:len(value)-len("Tracklist:")]
 					tagMap["type"] = "album"
-				} else if strings.HasSuffix(value, "Albums:"){
+				} else if strings.HasSuffix(value, "Albums:") {
 					value = value[:len(value)-len("Albums:")]
 					tagMap["type"] = "artist"
 				}
@@ -201,8 +195,8 @@ func (r Rutracker) parseTag(text string, tags []string) map[string]string {
 
 	}
 
-	for _, line := range lines{
-		if strings.Contains(line, "|") && strings.Contains(line, ":"){
+	for _, line := range lines {
+		if strings.Contains(line, "|") && strings.Contains(line, ":") {
 			subTags := strings.Split(line, "|")
 			for _, subTag := range subTags {
 				parseTag(subTag)
@@ -211,9 +205,9 @@ func (r Rutracker) parseTag(text string, tags []string) map[string]string {
 			tagMap["title"] = strings.TrimSpace(line[:index])
 		} else if index := strings.Index(line, "- дискография"); index >= 0 {
 			tagMap["title"] = strings.TrimSpace(line[:index])
-		} else if  index := strings.Index(line, "- Discography"); index >= 0 {
+		} else if index := strings.Index(line, "- Discography"); index >= 0 {
 			tagMap["title"] = strings.TrimSpace(line[:index])
-		} else if  index := strings.Index(line, "- discography"); index >= 0 {
+		} else if index := strings.Index(line, "- discography"); index >= 0 {
 			tagMap["title"] = strings.TrimSpace(line[:index])
 		} else {
 			parseTag(line)
@@ -223,10 +217,8 @@ func (r Rutracker) parseTag(text string, tags []string) map[string]string {
 	return tagMap
 }
 
-
-
 func (r Rutracker) ParseAlbumInfo(postBody *goquery.Selection) map[string]interface{} {
-	return r.parseSpWrapper(postBody.Children())
+	return r.parseSpWrapper(postBody.ChildrenFiltered(".sp-wrap"))
 }
 
 func (r Rutracker) parseSpWrapper(selection *goquery.Selection) map[string]interface{} {
@@ -234,9 +226,9 @@ func (r Rutracker) parseSpWrapper(selection *goquery.Selection) map[string]inter
 	selection.Each(func(i int, selection *goquery.Selection) {
 		// 如果之前有span
 
-		if selection.Find(".sp-body:only-child .sp-wrap").Size() > 0 {
+		if selection.Find(".sp-body .sp-wrap").Size() > 0 {
 			title := selection.Find("div.sp-head").First().Text()
-			subMap := r.parseSpWrapper(selection.Find(".sp-body .sp-wrap"))
+			subMap := r.parseSpWrapper(selection.Find(".sp-body").First().ChildrenFiltered(".sp-wrap"))
 			tempMap := make(map[string]interface{})
 			for key, value := range subMap {
 				tempMap[key] = value
@@ -249,10 +241,10 @@ func (r Rutracker) parseSpWrapper(selection *goquery.Selection) map[string]inter
 		sp_body := selection.Find("div.sp-body")
 		sp_text := sp_body.Text()
 		image, _ := sp_body.Find(".postImg").Attr("title")
-		totalmap[title]= model.ForumAlbumDetail{
-			Name: title,
+		totalmap[title] = model.ForumAlbumDetail{
+			Name:    title,
 			Content: sp_text,
-			Image: image,
+			Image:   image,
 		}
 	})
 	return totalmap
